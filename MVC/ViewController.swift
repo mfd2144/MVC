988ed195d7f780c52d92b4f3d370a141766c91e4
Fatih Dogan <fatih.dogan@laksi.com.tr>
2024-08-12 21:20:57 +0300

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Fatih Doğan on 12.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
